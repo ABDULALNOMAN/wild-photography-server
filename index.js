80b8f0bce8c202fa7b3ab80f88dcfc21ff9d0e36
unknown <nomanmd1st2001@gmail.com>
2022-11-08 11:38:25 +0600

    add data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -13,15 +14,23 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.USER_PASSWORD}@cluster0.sk6nr80.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const run = async() => {
+    try {
+        const wildCollection = client.db('wildWebData').collection('wild-collections')
+
+        app.get('/products', async(req, res) => {
+            const result = await wildCollection.find({}).limit(3).toArray()
+            res.send(result)
+        })
+    }
+    finally {
+        
+    }
+}
+run().catch(error=>console.log(error))
+
 
 
 app.get('/', (req, res) => {
