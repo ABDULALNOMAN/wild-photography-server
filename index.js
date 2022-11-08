5ec593d823594ded369f9697a23ce747bfe213d8
unknown <nomanmd1st2001@gmail.com>
2022-11-08 10:39:05 +0600

    mongodb add

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000
+
+
+// user :UserDb
+// pass : F2Swtr9ZR2gB1FlA
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.USER_PASSWORD}@cluster0.sk6nr80.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 app.get('/', (req, res) => {
