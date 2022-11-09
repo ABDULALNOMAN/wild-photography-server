a690e40e55c8488d3cd2ed352013158db5fa2026
unknown <nomanmd1st2001@gmail.com>
2022-11-09 10:27:24 +0600

    added review

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
-
-
-// user :UserDb
-// pass : F2Swtr9ZR2gB1FlA
 
 // middleware
 app.use(cors())
@@ -18,10 +14,30 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.USER_PASSWORD}@c
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const run = async() => {
     try {
-        const wildCollection = client.db('wildWebData').collection('wild-collections')
+        const wildCollection = client.db('traver').collection('tourist-services')
+        const userReviews = client.db('wildData').collection('reviewItem')
 
         app.get('/products', async(req, res) => {
             const result = await wildCollection.find({}).limit(3).toArray()
+            res.send(result)
+        })
+        app.get('/services', async(req, res) => {
+            const result = await wildCollection.find({}).toArray()
+            res.send(result)
+        })
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params
+            const query = { _id: ObjectId(id) }
+            const result = await wildCollection.findOne(query)
+            res.send(result)
+        })
+        app.post('/reviews', async(req, res) => {
+            const query = req.body
+            const result = await userReviews.insertOne(query)
+            res.send(result)
+        })
+        app.get('/getreviews', async(req, res) => {
+            const result = await userReviews.find({}).toArray()
             res.send(result)
         })
     }
